366cefa18746de7d9f2c161a4b6bb41c12505664
Rahul <rahul20107@iiitd.ac.in>
2024-08-12 21:22:13 +0530

    eighth commit

--- src/fatrowtable.js
+++ src/fatrowtable.js
@@ -53,6 +53,47 @@ const FatRowTable = () => {
                     <option value="server">Server</option>
                 </select>
             </div>
+
+            <table className="fat-row-table">
+                <thead>
+                    <tr>
+                        <th>Service</th>
+                        <th onClick={() => requestSort('client', 'requests')}>Client Requests</th>
+                        <th onClick={() => requestSort('client', 'rate')}>Client Rate</th>
+                        <th onClick={() => requestSort('client', 'p75')}>Client p75</th>
+                        <th onClick={() => requestSort('client', 'p90')}>Client p90</th>
+                        <th onClick={() => requestSort('client', 'p99')}>Client p99</th>
+                        <th onClick={() => requestSort('client', 'error')}>Client Error</th>
+                        <th onClick={() => requestSort('server', 'requests')}>Server Requests</th>
+                        <th onClick={() => requestSort('server', 'rate')}>Server Rate</th>
+                        <th onClick={() => requestSort('server', 'p75')}>Server p75</th>
+                        <th onClick={() => requestSort('server', 'p90')}>Server p90</th>
+                        <th onClick={() => requestSort('server', 'p99')}>Server p99</th>
+                        <th onClick={() => requestSort('server', 'error')}>Server Error</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    {filteredData.map((service, index) => (
+                        <React.Fragment key={index}>
+                            <tr>
+                                <td >{service.service}</td>
+                                <td>{service.client.requests}</td>
+                                <td>{service.client.rate}</td>
+                                <td>{service.client.p75}</td>
+                                <td>{service.client.p90}</td>
+                                <td>{service.client.p99}</td>
+                                <td>{service.client.error}</td>
+                                <td>{service.server.requests}</td>
+                                <td>{service.server.rate}</td>
+                                <td>{service.server.p75}</td>
+                                <td>{service.server.p90}</td>
+                                <td>{service.server.p99}</td>
+                                <td>{service.server.error}</td>
+                            </tr>
+                        </React.Fragment> ))}
+                </tbody>
+            </table>
         </div>
     );
 };
