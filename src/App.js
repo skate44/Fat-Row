0ded94fc04751f55c641c3fb909663ba26b6777f
Rahul <rahul20107@iiitd.ac.in>
2024-08-12 21:02:20 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './index.css';
+// import FatRowTable from './fatrowtable';
 
 function App() {
   return (
     <div className="App">
       <h1 style={{textAlign: 'center', marginTop: '20px'}}>Service Metrics Table</h1>
+      {/* <FatRowTable /> */}
     </div>
   );
 }
