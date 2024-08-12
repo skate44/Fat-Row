8b6dfb764e8bee3c9ede814ffc6b81021723a833
Rahul <rahul20107@iiitd.ac.in>
2024-08-12 21:07:57 +0530

    sixth commit

---
+++ src/fatrowtable.js
@@ -0,0 +1,15 @@
+import './index.css'
+// import React, { useState } from 'react';
+// import { servicesData } from './data';
+
+const FatRowTable = () => {
+
+
+    return (
+        <div className="table-container">
+
+        </div>
+    );
+};
+
+export default FatRowTable;
